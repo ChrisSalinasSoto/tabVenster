19b3b794f07d99a5edda3a360e91c0b7ce205614
ChrisSalinasSoto <SSSillyMac@MacBook-Pro-van-Christofer.local>
2016-02-15 11:34:37 +0100

    deel

---
+++ script.js
@@ -0,0 +1,26 @@
+$('.knoppen').each(function(){
+    var dezeKnop = $(this);
+    var actieveKnop = dezeKnop.find('li.actief');
+    var link = actieveKnop.find('a');
+    var actiefPaneel = $(link.attr('href'));
+    
+    dezeKnop.on('click', 'a', function(e){
+        e.preventDefault();
+        var link = $(this);
+       // alert(link.attr('href'));
+        var beoogId = this.hash;
+        //alert(beoogId);
+        
+        if(beoogId && !link.parent().is('.actief')){
+            actiefPaneel.removeClass('actief');
+            actieveKnop.removeClass('actief');
+            
+            actiefPaneel = $(beoogId).addClass('actief');
+            actieveKnop = link.parent().addClass('actief');
+        }    
+    })
+});
+
+// unbrusive weghalen
+
+$(".paneel").css('display', 'none')
